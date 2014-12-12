da1c4bd0b3bfaafca71dd6222c67f636c3d90687
Jewgeni Rose <j.rose@tu-bs.de>
2014-12-12 10:51:36 +0100

    init commit

---
+++ model/erschema.cs
@@ -0,0 +1,13 @@
+SYNTAXDEF erm
+FOR <http://tu-bs.de/cs/isf/mbse/erschema> <erschema.genmodel>
+START Schema
+
+OPTIONS {
+	reloadGeneratorModel = "true";
+	generateCodeFromGeneratorModel = "true";
+}
+
+RULES {
+	// TODO write rules
+	Schema   ::= "model";	
+}
---
+++ src/de/tu_bs/cs/isf/mbse/erm/generator/CodeGenerator.java
@@ -0,0 +1,10 @@
+package de.tu_bs.cs.isf.mbse.erm.generator;
+
+public final class CodeGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
